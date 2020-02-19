9a370009f9fc8fdeae1b1ee0b424f548ebe4547d
Cory Rekieta <coryrekieta@hotmail.com>
2020-02-19 07:21:08 -0600

    Added linked customer

--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -10,10 +10,12 @@ namespace MyShop.WebUI.Controllers
 {
     public class BasketController : Controller
     {
+        IRepository<Customer> customers;
         IBasketService basketService;
         IOrderService orderService;
-        public BasketController(IBasketService BasketService, IOrderService OrderService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService, IRepository<Customer> Customers)
         {
+            this.customers = Customers;
             this.basketService = BasketService;
             this.orderService = OrderService;
         }
@@ -38,15 +40,36 @@ namespace MyShop.WebUI.Controllers
             var basketSummary = basketService.GetBasketSummary(this.HttpContext);
             return PartialView(basketSummary);
         }
+        [Authorize]
         public ActionResult CheckOut()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    Surname = customer.LastName,
+                    ZipCode = customer.ZipCode 
+                };
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
         [HttpPost]
+        [Authorize]
         public ActionResult CheckOut(Order order)
         {
             var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
             //process payment
             order.OrderStatus = "Payment Processed";
             orderService.CreateOrder(order, basketItems);
